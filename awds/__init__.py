ca59395dc3c02139eb49919cd3085ad204dbb85f
Ivan <110347166+icevan96@users.noreply.github.com>
2025-04-28 11:56:32 +0200

    Delete awds directory

--- awds/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-AWDS: Automatic Whistler Detection System
-----------------------------------------
-
-A Python package for detecting whistler waves in satellite data.
-"""
-
-__version__ = '0.1.0'
